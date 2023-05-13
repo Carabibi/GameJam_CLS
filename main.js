f903213c5cfb13ad252ce1f836ede4715dc6c7cb
Carabibi <carabistouillesGJ@gmail.com>
2023-05-14 01:24:26 +0200

    ShurikenFUM4

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var config = {
         }
     },
     input: { gamepad: true },
-    scene: [Menu,scene1,scene4],
+    scene: [Menu,scene1,scene2,scene3,scene4],
     pixelArt : true ,
 } 
 var game =  new Phaser.Game(config);
