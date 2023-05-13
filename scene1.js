2ce4a0c5a321e10a611170cf2b0ccd00b486a8bc
Carabistouille <133488014+Carabibi@users.noreply.github.com>
2023-05-13 16:09:09 +0200

    Update scene1.js

--- scene1.js
+++ scene1.js
@@ -1,11 +1,16 @@
 var CDDash = true
 var HPmax = 100
 var HP = 100
+var degat =34
+var vitessedep=1
+var vitessedatk=1
+var speedatk=1
+
 class scene1 extends Phaser.Scene {
     constructor() {
         super('scene1');
         this.CanShoot = true;
-
+    
     }
 
     init(data) {
@@ -13,7 +18,7 @@ class scene1 extends Phaser.Scene {
     preload() {   
         this.load.tilemapTiledJSON("map", "assets/map.json");
         this.load.image("tileset", "assets/placeholder.png");
-        this.load.spritesheet('perso',"assets/perso.png",{frameWidth:61,frameHeight:47})
+        this.load.spritesheet('perso',"assets/perso.png",{frameWidth:47,frameHeight:61})
         this.load.spritesheet('shuriken','assets/Shuriken-sheet.png',{frameWidth:16,frameHeight:16})
         this.load.spritesheet('HP','assets/HPBar180x37.png',{frameWidth:180,frameHeight:37})
     }
@@ -102,8 +107,27 @@ class scene1 extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('shuriken', {start: 0, end: 3}),
             frameRate: 20,
         });
-
-
+        
+        this.anims.create({
+            key:'anim droite',
+            frames: this.anims.generateFrameNumbers('perso',{start:6,end:8 }),
+            frameRate:20,
+        })
+        this.anims.create({
+            key:'anim gauche',
+            frames: this.anims.generateFrameNumbers('perso',{start:9,end:11 }),
+            frameRate:20,
+        })
+        this.anims.create({
+            key:'anim face',
+            frames: this.anims.generateFrameNumbers('perso',{start:0,end:2 }),
+            frameRate:20,
+        })
+        this.anims.create({
+            key:'anim dos',
+            frames: this.anims.generateFrameNumbers('perso',{start:3,end:5 }),
+            frameRate:20,
+        })
   
     }
 
@@ -112,7 +136,8 @@ class scene1 extends Phaser.Scene {
         //                           DEPLACEMENT JOUEUR
         // Droite/gauche
         if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160)
+            this.player.setVelocityX(-160*vitessedep)
+            this.player.anims.play('anim gauche',true)
             if (this.clavier.SHIFT.isDown && CDDash == true) {
                 this.player.setVelocityX(-800)
                 setTimeout(() => {
@@ -125,7 +150,8 @@ class scene1 extends Phaser.Scene {
 
         }
         else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160);
+            this.player.setVelocityX(160*vitessedep);
+            this.player.anims.play('anim droite',true)
             if (this.clavier.SHIFT.isDown && CDDash == true) {
                 this.player.setVelocityX(800)
                 setTimeout(() => {
@@ -141,7 +167,8 @@ class scene1 extends Phaser.Scene {
         }
         // Haut/bas
         if (this.cursors.up.isDown) {
-            this.player.setVelocityY(-160)
+            this.player.setVelocityY(-160*vitessedep)
+            this.player.anims.play('anim dos',true)
             if (this.clavier.SHIFT.isDown && CDDash == true) {
                 this.player.setVelocityY(-800)
                 setTimeout(() => {
@@ -153,7 +180,8 @@ class scene1 extends Phaser.Scene {
             }
         }
         else if (this.cursors.down.isDown) {
-            this.player.setVelocityY(160)
+            this.player.setVelocityY(160*vitessedep)
+            this.player.anims.play('anim face',true)
             if (this.clavier.SHIFT.isDown && CDDash == true) {
                 this.player.setVelocityY(800)
                 setTimeout(() => {
@@ -180,21 +208,21 @@ class scene1 extends Phaser.Scene {
         
         if (this.clavier.E.isDown && this.CanShoot == true) {
             if(this.cursors.right.isDown){
-                this.shuriken.create(this.player.x + 50, this.player.y, "shuriken").setVelocityX(600);   
+                this.shuriken.create(this.player.x + 50, this.player.y, "shuriken").setVelocityX(475*speedatk);   
             }
             else if(this.cursors.left.isDown){
-                this.shuriken.create(this.player.x + 50, this.player.y, "shuriken").setVelocityX(-600);
+                this.shuriken.create(this.player.x + 50, this.player.y, "shuriken").setVelocityX(-475*speedatk);
             }
             else if(this.cursors.up.isDown){
-                this.shuriken.create(this.player.x + 50, this.player.y, "shuriken").setVelocityY(-600);
+                this.shuriken.create(this.player.x + 50, this.player.y, "shuriken").setVelocityY(-475*speedatk);
             }
             else if(this.cursors.down.isDown){
-                this.shuriken.create(this.player.x + 50, this.player.y, "shuriken").setVelocityY(600);
+                this.shuriken.create(this.player.x + 50, this.player.y, "shuriken").setVelocityY(475*speedatk);
             }
             this.CanShoot = false;
             setTimeout(() => {
                 this.CanShoot = true;
-            }, 500);
+            }, 500*vitessedatk);
         }  
         this.shuriken.getChildren().forEach(function(child){
             child.anims.play('shu', true)
