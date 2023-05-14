978da92e03b8afe929f68fcd42d6781decd04ea7
Barnabuss <carabistouillesGJ@gmail.com>
2023-05-14 14:17:14 +0200

    Update niveau3.js

--- niveau3.js
+++ niveau3.js
@@ -108,12 +108,15 @@ class niveau3 extends Phaser.Scene {
         this.GroupeEnnemi = this.physics.add.group({ immovable: true, allowGravity: false })
         this.EnnemiUn = this.GroupeEnnemi.create( 10 * 64 , 2 * 64, 'ennemie1');
         this.EnnemiUn_HP= 100
+<<<<<<< HEAD
 
         this.EnnemiDeux = this.GroupeEnnemi.create( 9 * 64 , 2 * 64, 'ennemie1');
         this.EnnemiDeux_HP= 100
 
         this.EnnemiTrois = this.GroupeEnnemi.create( 8 * 64 , 2 * 64, 'ennemie1');
         this.EnnemiTrois_HP= 100
+=======
+>>>>>>> parent of 86a047a (Update niveau3.js)
         //const ennemies = this.createEnemies()
 
 
